84cef65c74b47eacf2000e77fe3260d5d5ab4452
Victor Yao <victoryao1039@gmail.com>
2024-01-29 04:36:19 +0000

    test http methods

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 const http = require("http");
 
 const server = http.createServer();
-server.on("listening", () => "Listening...");
+server.on("listening", () => console.log("Listening..."));
 server.on("request", (req, res) => {
-	res.setHeader("Content-type", "application/json");
+	res.setHeader("Content-type", "text/plain");
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.writeHead(200);
 
-	res.end(JSON.stringify({ "testing": 123 }));
+	let resBody;
+
+	switch(req.method.toUpperCase()) {
+		case "GET":
+			resBody = "GET response";
+			res.writeHead(200);
+			break;
+		case "POST":
+			resBody = "POST response";
+			res.writeHead(200);
+			break;
+		default:
+			resBody = "Invalid request";
+			res.writeHead(400);
+			break;
+	}
+	
+	res.end(resBody);
 });
 
-server.listen(1919, () => {
-	console.log("Listening on 1919...");
-});
-
+server.listen(1919);
