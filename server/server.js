cbdcb582baa7c558f194c6c65234ecce03bbcbed
Victor Yao <victoryao1039@gmail.com>
2024-01-30 21:03:55 -0800

    test formidable

--- server/server.js
+++ server/server.js
@@ -7,26 +7,27 @@ server.on("request", (req, res) => {
 	res.setHeader("Content-type", "text/plain");
 	res.setHeader("Access-Control-Allow-Origin", "*");
 
-	let resCode = 400;
-	let resBody = "Invalid request";
-
 	switch(req.method.toUpperCase()) {
 		case "GET":
-			resCode = 200;
-			resBody = "GET response";
+			res.writeHead(200).end("GET response");
 			break;
 		case "POST":
 			if (req.url === "/up" || res.getHeader("Content-type") === "multipart/form-data") {
 				handleForm(req, res);
 			}
 			break;
+		default:
+			res.writeHead(404).end("Not found");
+			break;
 	}
-	
-	res.writeHead(resCode).end(resBody);
 });
 
 server.listen(1919);
 
 let handleForm = (req, res) => {
+	let form = new formidable.IncomingForm();
 
+	form.parse(req, (err, fields, files) => {
+		res.writeHead(200).end(JSON.stringify({fields, files}));
+	});
 };
