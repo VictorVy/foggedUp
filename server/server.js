58b0ed5ce4a8b726bd9fd1724eb6604167ced4fd
Victor Yao <victoryao1039@gmail.com>
2024-02-02 05:34:04 +0000

    bash head on 405 wall

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const formidable = require("formidable");
 const server = http.createServer();
 server.on("listening", () => console.log("Listening..."));
 server.on("request", (req, res) => {
+
+	console.log("check0");
 	res.setHeader("Content-type", "text/plain");
 	res.setHeader("Access-Control-Allow-Origin", "*");
 
@@ -13,9 +15,19 @@ server.on("request", (req, res) => {
 			res.writeHead(200).end("GET response");
 			break;
 		case "POST":
-			if (req.url === "/up" || res.getHeader("Content-type") === "multipart/form-data") {
-				handleForm(req, res);
-			}
+			//if (req.url === "/up" || res.getHeader("Content-type") === "multipart/form-data") {
+				//handleForm(req, res);
+			//}
+
+			console.log("check1");
+			let form = new formidable.IncomingForm();
+
+			console.log("check2");
+			form.parse(req, (err, fields, files) => {
+				res.writeHead(200).end(JSON.stringify({fields, files}));
+			});
+
+			console.log("check3");
 			break;
 		default:
 			res.writeHead(404).end("Not found");
@@ -31,5 +43,6 @@ let handleForm = (req, res) => {
 	form.parse(req, (err, fields, files) => {
 		res.writeHead(200).end(JSON.stringify({fields, files}));
 	});
+
 };
 
