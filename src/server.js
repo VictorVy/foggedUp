14526bfb56304fd85507baec99b39b32c0638412
Victor Yao <victoryao1039@gmail.com>
2024-02-15 05:43:31 +0000

    test https, ran into cert perms issue

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
-import { createServer } from "http";
+import { createServer as createHTTPServer } from "http";
+import { createServer as createHTTPSServer } from "https";
+import fs from "fs";
 import { IncomingForm } from "formidable";
 
-const server = createServer();
-server.on("listening", () => console.log("Listening..."));
+const server = createHTTPServer();
+
+server.on("listening", () => console.log("HTTP listening..."));
+
 server.on("request", (req, res) => {
 	res.setHeader("Content-type", "text/plain");
 	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	console.log("Request method: " + req.method + "\nRequest URL: " + req.url + "\nRequest headers:");
+	console.log(req.headers);
 
 	switch(req.method.toUpperCase()) {
 		case "GET":
@@ -22,8 +29,6 @@ server.on("request", (req, res) => {
 	}
 });
 
-server.listen(1919);
-
 let handleForm = (req, res) => {
 	let form = new IncomingForm();
 
@@ -32,3 +37,39 @@ let handleForm = (req, res) => {
 	});
 };
 
+server.listen(1919);
+
+// HTTPS TEST
+
+const options = {
+	key: fs.readFileSync("/etc/letsencrypt/live/fog.victoryao.com/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/fog.victoryao.com/fullchain.pem")
+};
+
+const httpsServer = createHTTPSServer(options);
+
+httpsServer.on("listening", () => console.log("HTTPS listening..."));
+
+httpsServer.on("request", (req, res) => {
+	res.setHeader("Content-type", "text/plain");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	console.log("Request method: " + req.method + "\nRequest URL: " + req.url + "\nRequest headers:");
+	console.log(req.headers);
+
+	switch(req.method.toUpperCase()) {
+		case "GET":
+			res.writeHead(200).end("GET response");
+			break;
+		case "POST":
+			if (req.url === "/up" || res.getHeader("Content-type") === "multipart/form-data") {
+				handleForm(req, res);
+			}
+			break;
+		default:
+			res.writeHead(404).end("Not found");
+			break;
+	}
+});
+
+httpsServer.listen(4242);
